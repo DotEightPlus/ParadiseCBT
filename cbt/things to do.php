46108a3d69fe5cc27658790915c3c31db7a51166
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-04-02 14:24:17 +0100

    deleted

--- cbt/things to do.php
+++
@@ -1,4 +0,0 @@
-on start of the cbt, shuffle the questions
-get the number of questions needed to be attempted
-create a table and save the questions to the table with the number of questions to be attempted
-on submit, save result and drop the table
